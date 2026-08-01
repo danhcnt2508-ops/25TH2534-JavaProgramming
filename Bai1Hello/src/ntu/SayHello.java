7ce18f940a6144f304ca43d81cfa40888881d80d
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-01 13:02:20 +0700

    Vi du dau tien
    Chỉ để demo

---
+++ Bai1Hello/src/ntu/SayHello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ntu;
+
+/**
+ * 
+ */
+public class SayHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Lần đâu tôi học Java, và đang thử nghiệm lưu mã nguồn với Git");
+	}
+
+}
