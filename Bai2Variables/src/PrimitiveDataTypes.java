3eeae94d68437a069a4a9e9cb03dd23bbea012e2
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-01 23:59:22 +0700

    Update PrimitiveDataTypes
    kiểu số thực
    kiểu ký tự
    kiểu boolean

--- Bai2Variables/src/PrimitiveDataTypes.java
+++ Bai2Variables/src/PrimitiveDataTypes.java
@@ -1,10 +1,4 @@
-/**
- * 
- */
 
-/**
- * 
- */
 public class PrimitiveDataTypes {
 
 	/**
@@ -22,7 +16,30 @@ public class PrimitiveDataTypes {
 		System.out.println(tongDanSo);
 		
 		//kiểu số thực
-
+		float diemTrungBinh = 8.5f; //f chỉ kiểu float
+		double pi = 3.141592653589793;
+		double canBacHai = Math.sqrt(25);
+		System.out.println(diemTrungBinh);
+		System.out.println(pi);
+		System.out.println(canBacHai);
+		
+		//kiểu ký tự
+		char kyTu = 'a';
+		char kyTuUnicode = '\u0061';
+		char kyTuSo = 97;
+		char kyTuDacBiet = '\n';
+		System.out.println(kyTu);
+		System.out.println(kyTuUnicode);
+		System.out.println(kyTuSo);
+		System.out.println(kyTuDacBiet);
+		
+		//kiểu boolean
+		boolean isJavaFun = true;
+		boolean isFishTasty = false;
+		boolean isAdult = (tuoi >=18);
+		System.out.println(isJavaFun);
+		System.out.println(isFishTasty);
+		System.out.println(isAdult);
 	}
 
 }
