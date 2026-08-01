54311c60c4a937496836c665deaed75a6ee0cf90
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-01 20:02:52 +0700

    Tinh toan co ban
    4 phep tinh: + - * /

---
+++ Bai1Hello/src/ntu/TinhToan.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ntu;
+
+/**
+ * 
+ */
+public class TinhToan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,y=4;
+		x=2+3;
+		System.out.println(x);
+		x=100-23;
+		System.out.println(x);
+		y=y*15;
+		System.out.println(y);
+		y=x+ y/3;
+		System.out.println(y);
+	}
+
+}
