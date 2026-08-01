de6a867394417345143e3332ea235deac7d61042
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-01 22:15:32 +0700

    Bài 2 Biến (kiểu và khai báo)

---
+++ Bai2Variables/src/PrimitiveDataTypes.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class PrimitiveDataTypes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//kiểu số nguyên
+		byte tuoi = 25;
+		short soLuong = 1000;
+		int danSo = 1000000;
+		long tongDanSo = 8000000000L;
+		System.out.println(tuoi);
+		System.out.println(soLuong);
+		System.out.println(danSo);
+		System.out.println(tongDanSo);
+		
+		//kiểu số thực
+
+	}
+
+}
