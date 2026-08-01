02bba401a73bc94e4e9a36e48df0127d1f19a0a3
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-01 17:30:27 +0700

    Thêm 2 ví dụ
    HelloWorld và HinhVe

---
+++ Bai1Hello/src/ntu/HelloWWorld.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package ntu;
+
+/**
+ * 
+ */
+public class HelloWWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Text blocks với """
+		String greeting = """
+				=======================
+				HELLO, MODERN JAVA!
+				=======================
+				by Mai Cường Thọ NTU
+				""";
+		System.out.println(greeting);
+	}
+
+}
--- Bai1Hello/src/ntu/SayHello.java
+++ Bai1Hello/src/ntu/SayHello.java
@@ -15,6 +15,7 @@ public class SayHello {
 		// TODO Auto-generated method stub
 		System.out.println("Lần đâu tôi học java, và đang thử nghiệm lưu mã nguồn với GitHub");
 		System.out.println("Tôi thêm 1 dòng và sửa chữ java ở trên");
+		System.out.println("Tôi tên Trần Mộng Danh - MSSV: 25TH2534");
 	}
 
 }
