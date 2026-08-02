6de3073846299f9c786247de2d5fb1da76e751dc
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-02 23:38:26 +0700

    Update Bài 3 Phần I

---
+++ Bai3.InOutData/src/OutputBasic.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class OutputBasic {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//In không xuống dòng
+		System.out.print("Xin chào ");
+		System.out.print("Java!");
+		
+		//kết quả: Xin chào Java!
+		
+		//In và xuống dòng
+		System.out.println("Cài đặt Android Studio,");
+		System.out.println("kèm máy ảo sẽ tốn khá nhiều dung lượng");
+		
+		//Xuống dòng rỗng
+		System.out.println();
+		System.out.println("Sau dòng trống");
+
+	}
+
+}
