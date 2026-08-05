bab0da6a44eb3f572741833752653b40f17b5fea
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-05 15:09:11 +0700

    Update BaitapThucHanh Bai2
    Phương trình bậc 1

--- Bai2_BaiTapVeNha/src/Phuongtrinhbac1.java
+++ Bai2_BaiTapVeNha/src/Phuongtrinhbac1.java
@@ -15,19 +15,19 @@ public class Phuongtrinhbac1 {
 		Scanner scanner = new Scanner(System.in);
 		//Nhập biến
 		System.out.print("Nhập vào giá trị a: ");
-		int a = scanner.nextInt();
+		double a = scanner.nextDouble();
 		System.out.print("Nhập vào giá trị b: ");
-		int b = scanner.nextInt();
+		double b = scanner.nextDouble();
 		double x;
 		
-		//Giải phương trình
+		//Giải phương trình ,
 		if(a != 0) {
-			x = -b/a;
-			System.out.println("Phương trình có nghiệm là %,.2", x)
-		} else if(b = 0) {
-			
-		}
+		 x = -b/a;
+		 System.out.printf("Nghiệm của phương trình là: %,.2f", x);
+		} else if(b == 0) {
+			System.out.println("Phương trình vô số nghiệm.");
+		} else System.out.println("Phương trình vô nghiệm");
 		
+		scanner.close();
 	}
-
 }
