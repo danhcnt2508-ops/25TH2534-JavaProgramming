5303a82ebe3f340aac7cad548baf50022d76ec56
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-16 01:50:16 +0700

    Update LoopConparison

--- Stream_API/src/LoopComparison.java
+++ Stream_API/src/LoopComparison.java
@@ -3,8 +3,23 @@ import java.util.List;
 public class LoopComparison {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<String> names = List.of("An", "Bình", "Châu", "Dũng");
+		
+		System.out.println("1. For truyền thống:");
+		for (int i = 0; i < names.size(); i++) {
+			System.out.println("- " + names.get(i));
+		}
+		
+		System.out.println("\n2. Enhanced for loop:");
+		for (String name : names) {
+			System.out.println("- " + name);
+		}
+		
+		System.out.println("\n3. Stream API với forEach:");
+		names.stream().forEach(name -> System.out.println("- " + name));
+		
+		System.out.println("\n4. Method reference:");
+		names.forEach(System.out :: println);
 	}
 
 }
