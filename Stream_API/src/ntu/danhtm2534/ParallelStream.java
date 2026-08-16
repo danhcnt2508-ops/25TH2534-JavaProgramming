1f803d84461a55fb119c4ec719a4b06b50fd1882
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-16 11:34:18 +0700

    update parallelStrwam

--- Stream_API/src/ntu/danhtm2534/ParallelStream.java
+++ Stream_API/src/ntu/danhtm2534/ParallelStream.java
@@ -1,10 +1,32 @@
 package ntu.danhtm2534;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class ParallelStream {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Tạo danh sách số lớn
+		List<Integer> bigList = IntStream.rangeClosed(1, 1000000)
+										 .boxed()
+										 .collect(java.util.stream.Collectors.toList());
+		long startTime, endTime;
+		
+		//Xử lý tuần tự
+		startTime = System.currentTimeMillis();
+		long sequentialSum = bigList.stream()
+									.mapToLong(Integer::longValue)
+									.sum();
+		endTime = System.currentTimeMillis();
+		System.out.println("Tuần tự: " + sequentialSum + " - Thời gian: " + (endTime -startTime) + "ms");
+		
+		//Xử lý song song
+		startTime = System.currentTimeMillis();
+		long parallelSum = bigList.parallelStream()
+								 .mapToLong(Integer::longValue)
+								 .sum();
+		endTime = System.currentTimeMillis();
+		System.out.println("Song song: " + parallelSum + " - Thời gian: " + (endTime - startTime) + "ms");
 	}
 
 }
