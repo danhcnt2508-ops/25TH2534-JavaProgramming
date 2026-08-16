1317255972f2aeae3cd473fc53fe0a4846395d7c
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-16 22:41:44 +0700

    Update OOP_TAIKHOAN_NGANHANG

--- OOP_TKNganHang/src/ntu/danhtm2534/TaiKhoanNganHang.java
+++ OOP_TKNganHang/src/ntu/danhtm2534/TaiKhoanNganHang.java
@@ -1,5 +1,35 @@
 package ntu.danhtm2534;
 
 public class TaiKhoanNganHang {
-
+	//tạo thuộc tính
+	String soTaiKhoan;
+	String tenChuTaiKhoan;
+	Double soDu;
+	
+	//tạo các phương thức
+	//Thêm tiền vào tài khoản
+	void guiTien(double soTien) {
+		soDu += soTien;
+	}
+	
+	//Rút tiền từ tài khoản
+	boolean rutTien(double soTien) {
+		if (soTien >= soDu)
+		return true;
+		else return false;
+	}
+	
+	
+	//Chuyển tiền sang tài khoản khác
+	double chuyenTien(TaiKhoanNganHang taiKhoanKhac, double soTien) {
+		
+	return soDu -= soTien;	
+	}
+	
+	
+	//hiển thị số dư hiện tại
+	void hienThiSoDu() {
+		System.out.println("Số dư hiện tại là: " + soDu + " đồng"); 
+	}
+	
 }
