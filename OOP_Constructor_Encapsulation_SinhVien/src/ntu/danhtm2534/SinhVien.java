cb177684e6ec98aedc9104863d4c7f432356817c
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-17 16:53:03 +0700

    update OOP_Constructor_Encapsulation_SINHVIEN

---
+++ OOP_Constructor_Encapsulation_SinhVien/src/ntu/danhtm2534/SinhVien.java
@@ -0,0 +1,48 @@
+package ntu.danhtm2534;
+
+public class SinhVien {
+	//thuộc tính private - chỉ truy cập trong lớp
+	private String ten;
+	private int tuoi;
+	private String maSV;
+	private double diemTB;
+	
+	//Constructor mặc định
+	public SinhVien() {
+		this.ten = "Chưa có tên";
+		this.tuoi = 18;
+		this.maSV = "SV000";
+		this.diemTB = 0.0;
+	}
+	
+	//Constructor có tham số
+	public SinhVien(String ten, int tuoi, String maSV) {
+		this.ten = ten;
+		this.setTuoi(tuoi); //sử dụng setter đển validate
+		this.maSV = maSV;
+		this.diemTB = 0.0;
+	}
+	
+	//Getter - lấy giá trị thuộc tính
+	public String getTen() {
+		return ten;
+	}
+	
+	public int getTuoi() {
+		return tuoi;
+	}
+	
+	public String getMaSV() {
+		return maSV;
+	}
+	
+	public double getDiemTB() {
+		return diemTB;
+	}
+	
+	//SETTER - Gán giá trị với kiểm tra
+	
+	
+	
+	
+}
