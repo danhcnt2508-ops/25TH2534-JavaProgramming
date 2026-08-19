34c43fc28ee2182ba1ff0a92673e63b1d82eda21
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-19 22:19:23 +0700

    update HeThongDongVatCB
    tạo lớp con: ConCho, ConMeo

--- HethongDongVatCB/src/ntu/danhtm2534/ConCho.java
+++ HethongDongVatCB/src/ntu/danhtm2534/ConCho.java
@@ -10,5 +10,28 @@ public class ConCho extends DongVat {
 	
 	
 	//Constructor của lớp con phải gọi constructor của lớp cha
+	public ConCho(String ten, int tuoi, double canNang, 
+			String giongCho, boolean coTheTrongNha) {
+		super(ten, tuoi, canNang);  //gọi constructor của DongVat
+		this.giongCho = giongCho;
+		this.coTheTrongNha = coTheTrongNha;
+	}
 	
+	//Phương thức riêng của Chó
+	public void sua() {
+		System.out.println(ten + " đang sủa: Gâu Gâu!");
+		
+	}
+	
+	public void duoiMeo() {
+		System.out.println(ten + " đang đuổi mèo...");
+	}
+	
+	//ghi đè phương thức của lớp cha
+	@Override
+	public void hienThiThongTin() {
+		super.hienThiThongTin(); // gọi phương thức của lớp cha
+		System.out.println("Giống chó: " + giongCho);
+		System.out.println("Có thể trông nhà: " + (coTheTrongNha));
+	}
 }
---
+++ HethongDongVatCB/src/ntu/danhtm2534/ConMeo.java
@@ -0,0 +1,7 @@
+package ntu.danhtm2534;
+
+//Lớp con: Mèo
+//File: ConMeo.java
+public class ConMeo {
+	
+}
