fe2202258f7360e76173c9b7ca2e883e09c70be3
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-19 18:18:44 +0700

    update Bai7.NhanVien, bai8.DongVat

--- Bai7.Encapsulation_NhanVien/src/ntu/danhtm2534/NhanVien.java
+++ Bai7.Encapsulation_NhanVien/src/ntu/danhtm2534/NhanVien.java
@@ -1,5 +1,22 @@
 package ntu.danhtm2534;
 
 public class NhanVien {
-
+	private String maNV;
+	private String hoTen;
+	private double luongCoBan;
+	private float heSoLuong;
+	
+	
+	//tạo constructor với đầy đủ thông tin
+	public NhanVien(String maNV, String hoTen, double luongCoBan, float heSoLuong) {
+		super();
+		this.maNV = maNV;
+		this.hoTen = hoTen;
+		this.luongCoBan = luongCoBan;
+		this.heSoLuong = heSoLuong;
+	}
+	
+	
+	
+	
 }
---
+++ HethongDongVatCB/src/ntu/danhtm2534/ConCho.java
@@ -0,0 +1,14 @@
+package ntu.danhtm2534;
+
+//Lớp con: Chó
+//File: ConCho.java
+
+public class ConCho extends DongVat {
+	//Thuộc tính riêng của Chó
+	private String giongCho;
+	private boolean coTheTrongNha;
+	
+	
+	//Constructor của lớp con phải gọi constructor của lớp cha
+	
+}
