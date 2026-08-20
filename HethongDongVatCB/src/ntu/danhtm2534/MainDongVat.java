982569633117c6bfbc2cac9c10773083e6d780a6
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-20 14:06:36 +0700

    update BT.HethongDongVat
    MainDongVat

--- HethongDongVatCB/src/ntu/danhtm2534/MainDongVat.java
+++ HethongDongVatCB/src/ntu/danhtm2534/MainDongVat.java
@@ -20,8 +20,29 @@ public class MainDongVat {
 		
 		//Tạo đối tượng mèo
 		ConMeo meo1 = new ConMeo("Mimi", 2, 4.3, "Tam thể", true);
-		
+		System.out.println("THÔNG TIN MÈO");
+		meo1.hienThiThongTin();
+		meo1.an();
+		meo1.ngu();
+		meo1.keu();
+		meo1.batChuot();
 
+		System.out.println("\n---\n");
+		
+		//Tính đa hình cơ bản: DongVat có thể tham chiếu đến đối tượng con
+		System.out.println("SỬ DỤNG TÍNH ĐA HÌNH: ");
+		DongVat dongVat1 = new ConCho("Max", 4, 12.0, "Poodle", false);
+		DongVat dongVat2 = new ConMeo("Tom", 1, 3.5, "Đen", false);
+		
+		dongVat1.an();		//Gọi phương thức của ConCho
+		dongVat2.an();		//Gọi phương thức của ConMeo (đã ghi đè)
+		
+		//Kiểm tra kiểu đối tượng
+		System.out.println("\n Kiểm tra kiểu đối tượng: ");
+		System.out.println("dongVat1 có phải là ConCho không? " +
+							(dongVat1 instanceof ConCho));
+		System.out.println("dongVat1 có phải là DongVat không? " + 
+							(dongVat1 instanceof DongVat));
 	}
 
 }
