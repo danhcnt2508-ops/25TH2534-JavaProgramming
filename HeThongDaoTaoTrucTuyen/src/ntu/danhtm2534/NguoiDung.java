93da09b67da6b313922dc18efd1d54098256b0e5
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-21 23:27:14 +0700

    update BT Bai8 HeThongDaoTaoTrucTuyen
    tạo class cha NguoiDung

---
+++ HeThongDaoTaoTrucTuyen/src/ntu/danhtm2534/NguoiDung.java
@@ -0,0 +1,32 @@
+package ntu.danhtm2534;
+
+//Lớp cha: Người dùng
+//File: NguoiDung.java
+public class NguoiDung {
+	protected String id;
+	protected String hoTen;
+	protected String email;
+	protected String ngayDangKy;
+	
+	
+	public NguoiDung(String id, String hoTen, String email, String ngayDangKy) {
+		super();
+		this.id = id;
+		this.hoTen = hoTen;
+		this.email = email;
+		this.ngayDangKy = ngayDangKy;
+	}
+	
+	//phương thức
+	public void dangNhap() {
+		System.out.println(hoTen + " đã đăng nhập hệ thống.");
+		return;
+	}
+	
+	public void xemThongTin() {
+		System.out.println("ID người dùng: ");
+		System.out.println("Họ tên: ");
+		System.out.println("Email: ");
+		System.out.println("Ngày đăng ký: ");
+	}
+}
