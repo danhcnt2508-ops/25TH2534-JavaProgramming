6e22225de9e2e80e4efa8213f397b7404c245858
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-22 21:15:29 +0700

    update BT HeThongVoHoc
    class VinhXuanQuyen

---
+++ HeThongVoHoc/src/ntu/danhtm2534/VinhXuanQuyen.java
@@ -0,0 +1,32 @@
+package ntu.danhtm2534;
+
+//Lớp con: Vịnh Xuân Quyền
+//File: VinhXuanQuyen.java
+public class VinhXuanQuyen extends VoHoc {
+	private boolean dungMocNhan;
+	private String baiQuyenNenTang;
+	
+	public VinhXuanQuyen(String tenMonVo, String nguonGoc, String trietLy, boolean dungMocNhan, String baiQuyenNenTang) {
+		super("Vịnh Xuân Quyền", "Trung Quốc", "Trung tuyến luận, cận chiến nhanh");
+		this.dungMocNhan = dungMocNhan;
+		this.baiQuyenNenTang = baiQuyenNenTang;
+		
+	}
+	
+	@Override
+	public void dacDiemChienDau() {
+		System.out.println("Đặc điểm: Sử dụng đấm liên hoàn đường thẳng, cận chiến tốc độ cao.");
+	}
+	
+	//thêm phương thức
+	public void tapMocNhan() {
+        if (dungMocNhan) {
+            System.out.println("Hành động: Luyện tập các góc đánh trên cọc gỗ mộc nhân với bài " + baiQuyenNenTang + ".");
+        }
+    }
+	
+	public void luyenChiSao() {
+        System.out.println("Hành động: Thực hành bài tập dính tay (Chi xao) cảm nhận lực đối thủ.");
+    }
+	
+}
