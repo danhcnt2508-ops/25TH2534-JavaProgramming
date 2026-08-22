50614bec0eca4b503ff470cdeab9a31fd036f8b1
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-22 12:59:33 +0700

    update class ThaiCucQuyen

---
+++ HeThongVoHoc/src/ntu/danhtm2534/ThaiCucQuyen.java
@@ -0,0 +1,38 @@
+package ntu.danhtm2534;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Lớp con: Thái cực quyền
+//File: ThaiCucQuyen.java
+public class ThaiCucQuyen extends VoHoc {
+	private List<String> dsChieuThuc;
+	private String tuyetChieu;
+	private boolean coTapThaiCucKiem;
+	
+	
+	public ThaiCucQuyen(String tenMonVo, String nguonGoc, String trietLy) {
+		super("Thái Cực Quyền", "Trung Quốc", "Lấy nhu khắc cương, mượn lực đánh lực");
+		this.dsChieuThuc = new ArrayList<>();
+		this.tuyetChieu = tuyetChieu;
+		this.coTapThaiCucKiem = coTapThaiCucKiem;
+	}
+	
+	@Override
+	public void dacDiemChienDau() {
+		System.out.println("Đặc điểm: Di chuyển chậm rãi, uyển chuyển, tập trung điều hòa hơi thở và khí công.");
+	} 
+	
+	//thêm phương thức
+	public void luyenKhiCong() {
+		System.out.println("Hành động: Đang luyện khí công và định tâm");
+	}
+	
+	public void bieuDienKiem() {
+		if (coTapThaiCucKiem) {
+            System.out.println("Hành động: Biểu diễn bài Thái Cực Kiếm với chiêu thức:" + dsChieuThuc);
+        } else {
+            System.out.println("Môn sinh này chỉ tập quyền cước không dùng kiếm.");
+        }
+	}
+}
--- HeThongVoHoc/src/ntu/danhtm2534/VoHoc.java
+++ HeThongVoHoc/src/ntu/danhtm2534/VoHoc.java
@@ -6,13 +6,49 @@ public class VoHoc {
 	protected String nguonGoc;
 	protected String trietLy;
 	
-	
+	//khởi tạo đầy đủ
 	public VoHoc(String tenMonVo, String nguonGoc, String trietLy) {
-		super();
 		this.tenMonVo = tenMonVo;
 		this.nguonGoc = nguonGoc;
 		this.trietLy = trietLy;
 	}
 	
-	//khởi tạo đầy đủ
+	//Phương thức hiển thị thông tin chung
+	public void hienThiThongTin() {
+		System.out.println("=== MÔN VÕ: " + tenMonVo.toUpperCase() + " ===");
+		System.out.println("Nguồn gốc: " + nguonGoc);
+		System.out.println("Triết lý: " + trietLy);
+	}
+	
+	//phương thức dacDiemChienDau
+	public void dacDiemChienDau() {
+		System.out.println("Mỗi môn võ có một đặc điểm chiến đấu riêng");
+	}
+
+	//Getter
+	public String getTenMonVo() {
+		return tenMonVo;
+	}
+
+	public String getNguonGoc() {
+		return nguonGoc;
+	}
+
+	public String getTrietLy() {
+		return trietLy;
+	}
+
+	// và Setter
+	public void setTenMonVo(String tenMonVo) {
+		this.tenMonVo = tenMonVo;
+	}
+
+	public void setNguonGoc(String nguonGoc) {
+		this.nguonGoc = nguonGoc;
+	}
+
+	public void setTrietLy(String trietLy) {
+		this.trietLy = trietLy;
+	}
+
 }
