f533d742ea55fdf4e14305f50dff5bdc87177a06
danhcnt2508-ops <danhcnt2508@gmail.com>
2026-08-22 12:37:28 +0700

    Bt Bai8 HeThongVoHoc

---
+++ HeThongVoHoc/src/ntu/danhtm2534/VoHoc.java
@@ -0,0 +1,18 @@
+package ntu.danhtm2534;
+
+public class VoHoc {
+	//thuộc tính chung
+	protected String tenMonVo;
+	protected String nguonGoc;
+	protected String trietLy;
+	
+	
+	public VoHoc(String tenMonVo, String nguonGoc, String trietLy) {
+		super();
+		this.tenMonVo = tenMonVo;
+		this.nguonGoc = nguonGoc;
+		this.trietLy = trietLy;
+	}
+	
+	//khởi tạo đầy đủ
+}
